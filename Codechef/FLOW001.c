cef27dc432377873f16ebe9dcb449c0148776c20
varun0308 <varunsathish038@gmail.com>
2021-05-06 20:55:53 +0530

    comitted

---
+++ Codechef/FLOW001.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,n;
+
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d %d",&a,&b);
+
+        printf("%d", a+b);
+    }
+    
+
+    return 0;
+}
